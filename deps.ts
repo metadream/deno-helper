4464a59c3d09c727af0a3c5e54972bcdc5bfd435
openflake <app@zerg.cc>
2023-02-28 20:06:02 +0800

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -3,4 +3,4 @@ export { serve } from "https://deno.land/std@0.177.0/http/server.ts";
 export { deleteCookie, getCookies, setCookie } from "https://deno.land/std@0.177.0/http/cookie.ts";
 export { extname, join, resolve } from "https://deno.land/std@0.177.0/path/mod.ts";
 export * as Reflect from "https://deno.land/x/reflection@0.0.2/mod.ts";
-export * as Tmplet from "https://esm.sh/tmplet@1.0.7/tmplet.ts";
+export * as Tmplet from "https://deno.land/x/tmplet@0.0.1/tmplet.ts";
