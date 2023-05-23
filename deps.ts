bf8b1fe1656ba0ca78dda19ea3fe50cb09883302
metadream <app@zerg.cc>
2023-05-23 09:38:00 +0800

    0.1.0

--- deps.ts
+++
@@ -1,7 +0,0 @@
-export { walk } from "https://deno.land/std@0.177.0/fs/mod.ts";
-export { serve } from "https://deno.land/std@0.177.0/http/server.ts";
-export type { ConnInfo } from "https://deno.land/std@0.177.0/http/server.ts";
-export { deleteCookie, getCookies, setCookie } from "https://deno.land/std@0.177.0/http/cookie.ts";
-export { extname, join, resolve } from "https://deno.land/std@0.177.0/path/mod.ts";
-export * as Reflect from "https://deno.land/x/reflection@0.0.2/mod.ts";
-export * as Tmplet from "https://deno.land/x/tmplet@0.0.1/tmplet.ts";
--- mod.ts
+++
@@ -1,6 +0,0 @@
-export * from "./decorator.ts";
-export { Context } from "./context.ts";
-export { Fragment, h } from "./jsx.ts";
-
-import { Server } from "./server.ts";
-export const core = new Server();
