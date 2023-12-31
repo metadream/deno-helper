281e73e37e0cff85c659e33ecc4dd3c7868035f5
metadream <app@zerg.cc>
2023-12-31 21:02:06 +0800

    Update util.ts

--- util.ts
+++ util.ts
@@ -46,8 +46,7 @@ export function formatDate(date: any, pattern: string, utc?: boolean): string {
  * @returns {String}
  */
 export function formatBytes(bytes: number): string {
-    if (!bytes) return "";
-    if (bytes < 1) return "0";
+    if (!bytes || bytes < 1) return "0";
     const unit = ["B", "K", "M", "G", "T", "P", "E", "Z"];
     const base = Math.min(unit.length - 1, Math.floor(Math.log(bytes) / Math.log(1024)));
     const scale = Math.max(0, base - 2);
